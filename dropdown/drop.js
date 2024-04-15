fdd461b8011a84c64fbf565aeda89b615a531980
Fardosa <muniiramohamed096@gmail.com>
2024-04-15 17:14:07 +0300

    my first commit

---
+++ dropdown/drop.js
@@ -0,0 +1,16 @@
+  let btn = document.querySelector(".fa-bars");
+  let sidebar = document.querySelector(".sidebar");
+
+
+  btn.addEventListener("click", ()=> {
+    sidebar.classList.toggle("close");
+  });
+
+
+  let arrows = document.querySelectorAll(".arrow");
+  for(var i = 0; i < arrows.length; i++){
+    arrows[i].addEventListener("click",(e) =>{
+    let arrowparent = e.target.parentElement.parentElement;
+    arrowparent.classList.toggle("show");
+    })
+  }
